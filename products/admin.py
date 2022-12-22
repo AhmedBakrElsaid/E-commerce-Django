9aac415b9816283eb894973a412dbe302d1cb3f1
AhmedBakrElsaid <Ahmedbakr24@std.mans.edu.eg>
2022-12-23 00:53:56 +0200

    add product model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Product,ProductImages,Brand,Category,ProductReview
 # Register your models here.
+
+class ProductImageTabular(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageTabular]
+    list_display = ['name','price','flag','quantity']
+    list_filter = ['flag','brand','category','price']
+    search_fields = ['name','desc','subtitle']
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(ProductImages)
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(ProductReview)
--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'orders',
     'settings',
     'products',
+    "taggit",
     
 ]
 
@@ -123,11 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media/"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / "media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
